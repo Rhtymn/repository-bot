a825ffbb5097ccf40fc19255cb3a8a38ce0e6a9b
Roihan <hroihan6@gmail.com>
2024-08-24 12:09:19 +0700

    create ddl.sql

---
+++ sql/ddl.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS db_repositoryapp;
+CREATE DATABASE db_repositoryapp;
+
+\c db_repositoryapp;
+
+CREATE TABLE users (
+	id BIGSERIAL PRIMARY KEY,
+	phone_number VARCHAR NOT NULL
+);
+
+
+CREATE TABLE directories (
+	id BIGSERIAL PRIMARY KEY,
+	title VARCHAR NOT NULL UNIQUE,
+	id_user BIGINT NOT NULL REFERENCES users(id)
+);
+
+
+CREATE TABLE links (
+	id BIGSERIAL PRIMARY KEY,
+	url VARCHAR NOT NULL,
+	title VARCHAR NOT NULL,
+	id_directory BIGINT NOT NULL REFERENCES directories(id)
+);
